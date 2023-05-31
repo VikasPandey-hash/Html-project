09b9b186a4e302e507c6ebb90767442b280c7020
mantoopandey <vikaspandey42@gmail.com>
2023-05-31 09:19:01 +0530

    done

---
+++ outputBased.js
@@ -0,0 +1,87 @@
+console.log(1+'2'+'2') // 122
+
+console.log(1-'1') //0
+
+console.log([] + []) ;//""
+
+console.log(true + false) // 1 
+
+console.log('b'+'a'+ +'a'+'a') //baNaNa
+
+console.log(undefined +2) // NaN
+
+console.log([]==[]) // false 
+
+console.log({}==={}) // false 
+
+console.log(NaN === NaN) //false 
+
+console.log(0.1+0.2 === 0.3) // false
+
+console.log(typeof typeof 1) ;  // string 
+
+console.log(2 * '3') //6 
+
+console.log(1<2<3) // true L=>R
+
+console.log(3>2>1) // false 1>1
+
+console.log('' == '0') // false
+
+console.log(0== '')  // true 
+
+console.log(0 == '0') // true 
+
+console.log([]==![]) // true 
+
+console.log(typeof NaN) ; // number 
+
+console.log((3).toString()) ; //3 
+
+console.log('hello'.length = 5) //5
+
+console.log(1+'1'-1) ; //10
+
+console.log(typeof null) // object
+
+console.log(2**3**2) ;//R=>L  512
+
+console.log(typeof []) ;// object 
+
+console.log('hello' -'hello') //NaN
+
+console.log([]+{})  // [object object]
+
+console.log(('Hello' || 0) && 'World') // world
+
+console.log(2 in [1,2]) ; // false
+
+console.log(null +1) ; //1 
+
+console.log('10' > '5') ; // false lexicographic conversion
+
+console.log(['10'] > ['5']) ; // true 
+
+console.log([1,2] + [3,4]) ; // '1,23,4'
+
+console.log(null + undefined) ; // NaN
+
+console.log("foo" instanceof String) // false 
+
+console.log(null === undefined) // true 
+
+console.log(+'') ; // 0
+
+console.log(!!'false')  // true 
+
+console.log([1] == true ) ; // false 
+
+console.log('Hello' + undefined) ; //'helloundefined'
+
+console.log(Math.max()) // -infinity
+
+console.log([1,2,3] instanceof Object) // true 
+
+
+
+
